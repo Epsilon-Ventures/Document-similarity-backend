ad23f0ff68b176484973999980df9c2528db712b
niteshswarnakar <nitesh.swarnakar1@gmail.com>
2023-02-17 12:08:47 +0545

    added database connection api inside views.py

--- backend/app/views.py
+++ backend/app/views.py
@@ -5,6 +5,11 @@ import numpy as np
 from rest_framework.decorators import api_view
 import json
 
+from pymilvus import connections, Collection, utility
+from sentence_transformers import SentenceTransformer
+
+
+
 
 def home(req):
     return HttpResponse("hello world")
@@ -34,7 +39,7 @@ def getModelResponse(req):
     response = requests.post(API_URL, headers = headers, json = payload1)
 
     result = np.array(response.json())
-    a = np.squeeze(result)
+    a = np.squeeze(result)  
     b_shape = np.mean(a,axis = 0)
     # print(b_shape)
     print(b_shape.shape)
@@ -67,6 +72,42 @@ def getSecondModel(req):
 
 
 
+def database_connection(req):
+    model = SentenceTransformer("Nischal2015/models_sbert_v6")
+
+    endpoint="https://in01-48ca8867d6ebb85.aws-us-west-2.vectordb.zillizcloud.com:19536"
+    connections.connect(
+    uri=endpoint,
+    secure=True,
+    user='kamao',
+    password='Random4545'
+    )
+    collection = Collection("questions")
+
+    question = ['What is a handoff and what are some various handoff detection techniques?']
 
 
+    embeddings = model.encode(question)
+    search_params = {
+        "metric_type": "L2",
+        "params": {"level": 1},
+    }
+    output_fields = ["question1"]
+
+
+    result = collection.search(
+    data = embeddings.tolist(),
+    anns_field="embeddings",
+    param=search_params,
+    limit=5,
+    )
+
+    ids = result[0].ids
+    res = collection.query(
+    expr = f"id in {ids}",   # id in [1, 3, 4, 5]
+    output_fields=output_fields,
+    consistency_level="Strong"
+    )
+    print(res)
+    return JsonResponse(res, safe = False)
 
